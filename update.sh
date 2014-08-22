311f72f66e42cf59b48c1df5aeb00a1282a232a3
Nils Schneider <nils@nilsschneider.net>
2014-08-22 22:14:13 +0200

    foo

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LC_ALL=en_US.UTF-8
 cd /home/nils/alfred2zone/
-curl -s http://metameute.de/~nils/alfred.json| /usr/bin/python3 alfred2zone.py > /var/lib/bind/nodes/nodes.ffhl.zone
+curl -s http://metameute.de/~freifunk/alfred/nodeinfo.json| /usr/bin/python3 alfred2zone.py > /var/lib/bind/nodes/nodes.ffhl.zone
 /usr/sbin/rndc reload
